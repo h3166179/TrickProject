e2442b4a7578ed8971e94e6939367c6b4176c03a
h3166179 <1178092718@qq.com>
2022-06-25 07:29:46 +0800

    Player Init

---
+++ TrickProject/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    public float height;
+
+    public float Speed;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, height);
+        }
+
+        rigidbody2D.velocity = new Vector2(h*Speed, rigidbody2D.velocity.y);
+    }
+}
