8f3fef9361bd8efc4334fc6bb56bdb73a8575dac
h3166179 <1178092718@qq.com>
2022-06-25 14:49:44 +0800

    no message

---
+++ TrickProject/Assets/Scripts/Manager/LevelEventManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class LevelEventManager 
+{
+    Dictionary<int, Action> LevelEventDic=new Dictionary<int, Action>();
+
+
+   public void RegisterLevelEvnent(int levelIndex,Action action)
+    {
+        if (!LevelEventDic.ContainsKey(levelIndex))
+        {
+            LevelEventDic.Add(levelIndex, action);
+        }
+    }
+}
