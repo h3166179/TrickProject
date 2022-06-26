8b7bdd0585941b3b0b3881079515cb4968d988f1
bear <995914739@qq.com>
2022-06-26 12:19:35 +0800

    #Update Sumbit

---
+++ TrickProject/Assets/Scripts/OtherEffect/EndTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTrigger : MonoBehaviour
+{
+    private SceneFader endfader;
+    [SerializeField] private Canvas healthUI;
+    [SerializeField] private Canvas endUI;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //GameManager.Instance.GetPlayer().GetComponent<PlayerController>().isMove = false;
+        StartCoroutine(Show());
+    }
+
+
+    private　IEnumerator Show()
+    {
+        GameManager.Instance.GetPlayer().gameObject.SetActive(false);
+        yield return new WaitForSeconds(5f);
+        endfader = Instantiate(Resources.Load<GameObject>("UI/Prefab/SceneFaderCanvas")).GetComponent<SceneFader>();
+        endfader.GetComponentInChildren<Image>().color = new Color(1, 1, 1, 1);
+        endfader.CanvasFadeIn();
+        yield return new WaitForSeconds(1f);
+        healthUI.gameObject.SetActive(false);
+        endUI.gameObject.SetActive(true);
+        endfader.CanvasFadeOut();
+    }
+}
